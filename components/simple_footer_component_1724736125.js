24cbb6679fc3f551824b020922af403f8832e5eb
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-27 05:22:11 +0000

    Placeholder text replacement

--- components/simple_footer_component_1724736125.js
+++ components/simple_footer_component_1724736125.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1724736125", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            WebSolve Pro
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        © 2023 WebSolve Pro. Empowering businesses with innovative web solutions.
                     </div>
                 </div>
             </div>
